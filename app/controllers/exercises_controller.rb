21d82c1ae9b467413a3cc17e998dc706483b39d2
amclean1 <lex213075@aim.com>
2015-10-14 10:17:36 -0400

    added views for the exercises and routes

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,4 +1,14 @@
 class ExercisesController < ApplicationController
-
-
+	def five
+		
+	end 
+	
+	def imitation
+		
+	end
+	
+	def prompt 
+		
+	end
+	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/sessions', to: 'sessions#create'
   delete '/sessions', to: 'sessions#destroy'
+  get '/five', to: 'exercises#five'
+  get '/imitation', to: 'exercises#imitation'
+  get '/prompt', to: 'exercises#prompt'
 
   root 'users#new'
 end
