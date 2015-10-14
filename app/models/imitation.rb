c0d9cd26d5a508e77901f1f97b6500534fff61f6
Andy <abeaudoin2013@gmail.com>
2015-10-14 15:43:19 -0400

    Added ruby code for imitation exercise

--- app/models/imitation.rb
+++ app/models/imitation.rb
@@ -1,2 +1,35 @@
 class Imitation < ActiveRecord::Base
+	# imitation methods
+def document_reader(document)
+    the_text = document.downcase
+    sanitized_text = the_text.gsub("-/", " ").delete(",.")
+    split_words = sanitized_text.split 
+    return split_words
+end
+
+def input_reader(input)
+    the_text = input.downcase
+    sanitized_text = the_text.gsub("-", " ").delete(",.")
+    split_inputs = sanitized_text.split 
+    return split_inputs
+end
+
+def checker(words, inputs)
+    for word in words
+        index_of_words = words.index(word)
+        for input_word in inputs
+           index_of_inputs = inputs.index(input_word)
+            if index_of_words == index_of_inputs
+                if word == input_word 
+                    next
+                else 
+                    puts "Your input does not match"
+                    next
+                end
+            else
+                next
+            end
+        end
+    end
+end
 end
