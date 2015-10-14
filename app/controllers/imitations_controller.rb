51ec7c87a9391f0ee968cae84908eb13441f61a3
Andy <abeaudoin2013@gmail.com>
2015-10-14 19:07:47 -0400

    Got imitation code to compare two sets of information

--- app/controllers/imitations_controller.rb
+++ app/controllers/imitations_controller.rb
@@ -1,6 +1,12 @@
 class ImitationsController < ApplicationController
 
-	def show
-		@
+	def index
+		@document = Document.new
+		@piece = Imitation.pluck(:piece).shuffle[0]
+		blah = "The authorities began setting up roadblocks and checkpoints and deployed police officers and soldiers, in response to what the government called a 'wave of terrorism.'"
+		text = @piece
+		sanitized_piece = Imitation.document_reader(@piece)
+		sanitized_input = Imitation.input_reader(blah)
+		@checker = Imitation.checker(sanitized_piece, sanitized_input)
 	end
 end
